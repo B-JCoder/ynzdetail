8aee29528e529a4f207a226b271e6b4a53985973
Bilal Jadi <148371977+B-JCoder@users.noreply.github.com>
2025-08-22 10:02:17 -0700

    remove {/* Scroll Indicator */}

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -73,11 +73,8 @@ export default function HeroSection() {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-ynz-cyan rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-ynz-cyan rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
+      
+      
     </section>
   )
 }
