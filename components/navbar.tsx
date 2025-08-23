796e1e8b86e3b0498e03b5869cf9c956d317f0d3
Bilal Jadi <148371977+B-JCoder@users.noreply.github.com>
2025-08-23 11:19:23 -0700

    set nav links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,11 +19,11 @@ export default function Navbar() {
   }, [])
 
   const navItems = [
-    { name: "Home", href: "#home" },
-    { name: "About", href: "#about" },
-    { name: "Services", href: "#services" },
-    { name: "Gallery", href: "#gallery" },
-    { name: "Contact", href: "#contact" },
+    { name: "Home", href: "/#home" },
+    { name: "About", href: "/#about" },
+    { name: "Services", href: "/#services" },
+    { name: "Gallery", href: "/#gallery" },
+    { name: "Contact", href: "/#contact" },
   ]
 
   return (
